4069ed06741c3441cde2278ca7c12ef14e3f0f3f
leah-morrison <leah.morrison@unb.ca>
2024-03-20 10:27:12 -0300

    removed hard coding

--- imu_accel_test.py
+++ imu_accel_test.py
@@ -16,17 +16,27 @@ delta_t = 0.087 # measurements taken every 87 ms
 accel_hex_x = ["E307", "2308", "F107", "F707", "EF07", "1208", "8607", "E307", "CD07", "EF07", "9107", "AE07", "1A08", "0D08", "F507", "E507", "0F08", "EB07", "0208", "FE07", "AC07", "F007", "3B08", "1C08", "D807"]
 accel_hex_y = ['3800', '4E01', '9201', 'DC01', 'CB01', 'AA01', '6101', '3901', '2E01', 'BF01', '5C01', 'BA01', 'A501', '9510', 'C401', 'B701', 'F301', '3C02', '4E02', '9701', '7601', '5802', '3402', '1F02', 'DF01']
 accel_hex_z = ['290E', 'E20D', '050E', 'CF0D', 'F20D', 'E30D', '4F0E', '0C0E', '2D0E', 'F60D', '2D0E', '3C0E', '0E0E', '190E', '1D0E', '000E', 'E70D', 'EF0D', 'F00D', '130E', '140E', 'E50D', 'DD0D', 'EF0D', 'F70D']
-lsb_value = 16384 # lSB/g
+lsb_value = 16384 # LSB/g
 
 # making arrays of zero
 accel_dec_x = np.zeros(len(accel_hex_x))
 accel_dec_y = np.zeros(len(accel_hex_x))
 accel_dec_z = np.zeros(len(accel_hex_x))
+
 time_array = np.zeros(len(accel_hex_x))
 lsb_matrix = np.zeros(len(accel_hex_x))
-accel_decimal = np.zeros(len(accel_hex_x))
-vel_values = np.zeros(len(accel_hex_x))
-dist_values = np.zeros(len(accel_hex_x))
+
+accel_decimal_x = np.zeros(len(accel_hex_x))
+accel_decimal_y = np.zeros(len(accel_hex_x))
+accel_decimal_z = np.zeros(len(accel_hex_x))
+
+vel_values_x = np.zeros(len(accel_hex_x))
+vel_values_y = np.zeros(len(accel_hex_x))
+vel_values_z = np.zeros(len(accel_hex_x))
+
+dist_values_x = np.zeros(len(accel_hex_x))
+dist_values_y = np.zeros(len(accel_hex_x))
+dist_values_z = np.zeros(len(accel_hex_x))
 
 # converting acceleration measurements
 for i in range(len(time_array)):
@@ -65,25 +75,19 @@ accel_values_x = 9.81*accel_g_x # in m/s^2
 accel_values_y = 9.81*accel_g_y
 accel_values_z = 9.81*accel_g_z
 
-vel_values_x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-vel_values_y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-vel_values_z = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
 for i in range(len(accel_values_x) - 1):
     vel_values_x[i + 1] = delta_t*(accel_values_x[i] + accel_values_x[i + 1])/2
     vel_values_y[i + 1] = delta_t*(accel_values_y[i] + accel_values_y[i + 1])/2
     vel_values_z[i + 1] = delta_t*(accel_values_z[i] + accel_values_z[i + 1])/2
-
-dist_values_x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-dist_values_y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-dist_values_z = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 for i in range(len(accel_values_x) - 1):
     dist_values_x[i + 1] = delta_t*(vel_values_x[i] + vel_values_x[i + 1])/2
     dist_values_y[i + 1] = delta_t*(vel_values_y[i] + vel_values_y[i + 1])/2
     dist_values_z[i + 1] = delta_t*(vel_values_z[i] + vel_values_z[i + 1])/2
 
-total_distance = sqrt(sum(dist_values_x)**2 + sum(dist_values_y)**2 + sum(dist_values_z)**2)
+total_distance_x = sum(dist_values_x)
+total_distance_y = sum(dist_values_y)
+total_distance_z = sum(dist_values_z)
 
 # plotting results
 plot1 = plt.subplot2grid((3, 3), (0, 0), colspan=3) 
@@ -115,13 +119,6 @@ plot3.legend()
 plt.tight_layout() 
 plt.show() 
 
-print("Acceleration in x measured in g:")
-print(accel_g_x)
-print("Acceleration in x-axis:")
-print(accel_values_x)
-print("Velocity in x-axis:")
-print(vel_values_x)
-print("Distance in x-axis:")
-print(dist_values_x)
-print("Total distance: " + str(total_distance))
-print(time_array)
+print("Total distance travelled in the x-axis: " + str(total_distance_x) + " m")
+print("Total distance travelled in the y-axis: " + str(total_distance_y) + " m")
+print("Total distance travelled in the z-axis: " + str(total_distance_z) + " m")
